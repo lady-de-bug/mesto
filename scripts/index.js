5f3693b3e674ff4a55fefa918bb6ccb602062dd5
Elena Kondrateva <epkondrateva@yandex.ru>
2023-02-25 16:45:40 +0300

    Add six cards via js

--- scripts/index.js
+++ scripts/index.js
@@ -29,4 +29,50 @@ editProfileButton.addEventListener("click", openPopup);
 popupСloseIcon.addEventListener("click", closePopup);
 popupForm.addEventListener("submit", handleFormSubmit);
 
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    alt: "Горы Архыза летом.",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    alt: "Озеро Челябинска зимой.",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    alt: "Жилой массив состоящий из панельных домов.",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    alt: "Растительность у подножья горы.",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    alt: "Железная дорога, бегущая среди полей и  уходящая к горизонту.",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    alt: "Волны озера омывают скалистый берег.",
+  },
+];
 
+const elements = document.querySelector(".elements");
+
+initialCards.forEach(function (card) {
+  const newElement = elements
+    .querySelector("#elementTemlate")
+    .content.cloneNode(true);
+  const elementImage = newElement.querySelector(".element__image");
+  elementImage.setAttribute('src', card.link)
+  elementImage.setAttribute('alt', card.alt)
+  const elementNamePlace = newElement.querySelector(".element__name-place");
+  elementNamePlace.textContent = card.name;
+
+  elements.append(newElement);
+});
