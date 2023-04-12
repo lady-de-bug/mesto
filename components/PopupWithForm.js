e062d1c40532040003c187470d3a03cefca5b9f4
Elena Kondrateva <epkondrateva@yandex.ru>
2023-04-13 00:54:29 +0300

    Fix PopupWithForm

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,13 +1,14 @@
 import Popup from './Popup.js';
 export default class PopupWithForm extends Popup {
-  constructor(popupSelector, handleFormSubmit) {
+  constructor({ popupSelector, handleFormSubmit }) {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
     this._form = this._popupSelector.querySelector('.popup__form');
+
   }
 
   _getInputValues() {
-    this._inputList = this._form.querySelectorAll('.form__input');
+    this._inputList = this._form.querySelectorAll('.popup__input');
     this._formValues = {};
     this._inputList.forEach(
       (input) => (this._formValues[input.name] = input.value)
@@ -15,13 +16,14 @@ export default class PopupWithForm extends Popup {
 
     return this._formValues;
   }
-  
+
   setEventListeners() {
     super.setEventListeners();
     this._form.addEventListener('submit', (evt) => {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
       super.close();
+      this._form.reset();
     });
   }
   close() {
